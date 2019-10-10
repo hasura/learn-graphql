da63f80ed2fb3770f3a96d2ab6061be494f73333
Akshay Kadam <akshaykadam0904@gmail.com>
2019-10-10 12:24:04 +0530

    Convert Class Components to Functional components

--- services/homepage/src/components/Header.js
+++ services/homepage/src/components/Header.js
@@ -1,55 +1,54 @@
 import React from 'react';
 import '../styles/styles.scss';
+import { backendTutorial, frontendTutorial, mobileTutorial } from './AllState.js';
 import CommonNavBar from './CommonNavBar';
-import {frontendTutorial, backendTutorial, mobileTutorial} from './AllState.js'
-class TopBanner extends React.Component {
-  render() {
-    const logo = 'https://graphql-engine-cdn.hasura.io/learn-hasura/assets/homepage/logo.svg';
-    return (
-      <header className={'headerWrapper blueBgColor'}>
-        <nav className="navbar navbar-default navbarDefault">
-          <div className="container">
-            <div className="navbar-header navbarHeader">
-              <div className={'logoWrapper'}>
-                <a href="https://hasura.io/" target="_blank" rel="noopener noreferrer"><img className={'img-responsive'} src={logo} alt={'Hasura logo'} /></a>
-              </div>
-              <button type="button" className="navbar-toggle collapsed navbarToggle" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
-                <span className="sr-only">Toggle navigation</span>
-                <span className="icon-bar"></span>
-                <span className="icon-bar"></span>
-                <span className="icon-bar"></span>
-              </button>
+
+const TopBanner = () => {
+  const logo = 'https://graphql-engine-cdn.hasura.io/learn-hasura/assets/homepage/logo.svg';
+  return (
+    <header className={'headerWrapper blueBgColor'}>
+      <nav className="navbar navbar-default navbarDefault">
+        <div className="container">
+          <div className="navbar-header navbarHeader">
+            <div className={'logoWrapper'}>
+              <a href="https://hasura.io/" target="_blank" rel="noopener noreferrer"><img className={'img-responsive'} src={logo} alt={'Hasura logo'} /></a>
             </div>
-            <div id="navbar" className="navbar-collapse collapse">
-              <ul className="nav navbar-nav navbar-right navBarWrapper">
-                <CommonNavBar
-                id="frontend"
-                navTitle="FRONTEND"
-                title="Frontend Tutorials"
-                description="2 hour Frontend GraphQL Tutorial Series to teach you your favorite framework."
-                commonTutorial = {frontendTutorial}
-                />
-                <CommonNavBar
-                id="mobile"
-                navTitle="MOBILE"
-                title="Mobile Tutorials"
-                description="2 hour mobile GraphQL Tutorial Series to teach you your favorite framework."
-                commonTutorial={mobileTutorial}
-                />
-                <CommonNavBar
-                id="backend"
-                navTitle="BACKEND"
-                title="Backend Tutorials"
-                description="2 hour backend GraphQL Tutorial Series to teach you your favorite framework."
-                commonTutorial={backendTutorial}
-                />
-              </ul>
-            </div>
+            <button type="button" className="navbar-toggle collapsed navbarToggle" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+              <span className="sr-only">Toggle navigation</span>
+              <span className="icon-bar"></span>
+              <span className="icon-bar"></span>
+              <span className="icon-bar"></span>
+            </button>
           </div>
-        </nav>
-      </header>
-    );
-  }
+          <div id="navbar" className="navbar-collapse collapse">
+            <ul className="nav navbar-nav navbar-right navBarWrapper">
+              <CommonNavBar
+              id="frontend"
+              navTitle="FRONTEND"
+              title="Frontend Tutorials"
+              description="2 hour Frontend GraphQL Tutorial Series to teach you your favorite framework."
+              commonTutorial = {frontendTutorial}
+              />
+              <CommonNavBar
+              id="mobile"
+              navTitle="MOBILE"
+              title="Mobile Tutorials"
+              description="2 hour mobile GraphQL Tutorial Series to teach you your favorite framework."
+              commonTutorial={mobileTutorial}
+              />
+              <CommonNavBar
+              id="backend"
+              navTitle="BACKEND"
+              title="Backend Tutorials"
+              description="2 hour backend GraphQL Tutorial Series to teach you your favorite framework."
+              commonTutorial={backendTutorial}
+              />
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </header>
+  );
 }
 
 export default TopBanner;

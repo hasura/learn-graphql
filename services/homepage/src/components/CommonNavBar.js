a37c39c973982f3926637dd63d226ff85a54bad4
Akshay Kadam (A2K) <akshaykadam0904@gmail.com>
2019-10-11 08:38:48 +0530

    homepage: convert class components to functional

--- services/homepage/src/components/CommonNavBar.js
+++ services/homepage/src/components/CommonNavBar.js
@@ -1,55 +1,55 @@
-import React from 'react';
 import PropTypes from 'prop-types';
+import React from 'react';
 import '../styles/styles.scss';
-class CommonNavBar extends React.Component {
-  render() {
-    const dropPath = 'https://graphql-engine-cdn.hasura.io/learn-hasura/assets/homepage/dropdown-path.svg';
-    return (
-      <li className="dropdown">
-        <div className={'upArrow'}>
+
+const CommonNavBar = ({ id, navTitle, title, description, commonTutorial}) => {
+  const dropPath = 'https://graphql-engine-cdn.hasura.io/learn-hasura/assets/homepage/dropdown-path.svg';
+  return (
+    <li className="dropdown">
+      <div className={'upArrow'}>
+      </div>
+      {/* eslint-disable-next-line */}
+      <button type="button" id={id} className="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">{navTitle}</button>
+      <div aria-labelledby="frontend" className="dropdown-menu dropdownMenu">
+        <div className={'dropdownMenuBgImg'}>
+          <img className={'img-responsive'} src={dropPath} alt={'dropPath'} />
         </div>
-        {/* eslint-disable-next-line */}
-        <button type="button" id={this.props.id} className="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">{this.props.navTitle}</button>
-        <div aria-labelledby="frontend" className="dropdown-menu dropdownMenu">
-          <div className={'dropdownMenuBgImg'}>
-            <img className={'img-responsive'} src={dropPath} alt={'dropPath'} />
+        <div className={'col-md-6 col-sm-6 col-xs-12'}>
+          <div className={'menuTitle'}>
+            {title}
           </div>
-          <div className={'col-md-6 col-sm-6 col-xs-12'}>
-            <div className={'menuTitle'}>
-              {this.props.title}
-            </div>
-            <div className={'purpleLineSeperator'}>
-            </div>
-            <div className={'sectionDescription'}>
-              {this.props.description}
-            </div>
+          <div className={'purpleLineSeperator'}>
           </div>
-          <div className={'col-md-6 col-sm-6 col-xs-12'}>
-            <ul className={'dropdownUl'}>
-              {this.props.commonTutorial.map((item, key) => {
-                if(!item.comingSoon) {
-                  return (
-                    <a href={item.url} target={'_blank'}>
-                      <li key={item.url} className={item.bgClassName}>
-                          {item.name}
-                      </li>
-                    </a>
-                  );
-                } else {
-                  return (
-                    <li key={item.url} className={item.disableBgClassName + ' displayFlex comingSoonHover'}>
-                      {item.name} <div className={'circle'}></div> <span>Coming soon</span>
+          <div className={'sectionDescription'}>
+            {description}
+          </div>
+        </div>
+        <div className={'col-md-6 col-sm-6 col-xs-12'}>
+          <ul className={'dropdownUl'}>
+            {commonTutorial.map((item, key) => {
+              if(!item.comingSoon) {
+                return (
+                  <a href={item.url} target={'_blank'}>
+                    <li key={item.url} className={item.bgClassName}>
+                        {item.name}
                     </li>
-                  );
-                }
-              })}
-            </ul>
-          </div>
+                  </a>
+                );
+              } else {
+                return (
+                  <li key={item.url} className={item.disableBgClassName + ' displayFlex comingSoonHover'}>
+                    {item.name} <div className={'circle'}></div> <span>Coming soon</span>
+                  </li>
+                );
+              }
+            })}
+          </ul>
         </div>
-      </li>
-    );
-  }
+      </div>
+    </li>
+  );
 }
+
 CommonNavBar.propTypes = {
   id: PropTypes.string.isRequired,
   title: PropTypes.string.isRequired,
@@ -57,4 +57,5 @@ CommonNavBar.propTypes = {
   description: PropTypes.string.isRequired,
   commonTutorial: PropTypes.array.isRequired,
 }
+
 export default CommonNavBar;
--- services/homepage/src/components/SubscribeNewsletter.js
+++ services/homepage/src/components/SubscribeNewsletter.js
@@ -1,7 +1,7 @@
 import React from 'react';
+import SubscribeFrom from 'react-mailchimp-subscribe';
 import '../styles/styles.scss';
 
-import SubscribeFrom from 'react-mailchimp-subscribe';
 // TODO: update list
 const formProps = {
   action: '//hasura.us13.list-manage.com/subscribe/post?u=9b63e92a98ecdc99732456b0e&amp;id=f5c4f66bcf',
@@ -32,38 +32,36 @@ const formProps = {
 };
 
 
-class SubscribeNewsletter extends React.Component {
-  render() {
-    return (
-      <section className={'gradientBgColor commonSectionWrapper positionRel'}>
-        <div className={'container noPadd'}>
-          <div className={'topBannerWrapper col-md-12'}>
-            <div className={'col-md-6 col-sm-12 col-xs-12 noPadd'}>
-              <h1 className={'sectionHeader'}>
-                Subscribe to the Hasura newsletter
-              </h1>
-              <div className={'sectionDescription'}>
-                Join the community mailing list to receive updates about the product, company and our OSS work.
-              </div>
-              {/*
-              <div className={'notifyWrapper'}>
-                <div className={'sectionDescription notifyHeader'}>
-                  Subscribe for updates
-                </div>
-                <SubscribeFrom className={"notifyForm"} {...formProps}/>
-              </div>
-              */}
+const SubscribeNewsletter = () => {
+  return (
+    <section className={'gradientBgColor commonSectionWrapper positionRel'}>
+      <div className={'container noPadd'}>
+        <div className={'topBannerWrapper col-md-12'}>
+          <div className={'col-md-6 col-sm-12 col-xs-12 noPadd'}>
+            <h1 className={'sectionHeader'}>
+              Subscribe to the Hasura newsletter
+            </h1>
+            <div className={'sectionDescription'}>
+              Join the community mailing list to receive updates about the product, company and our OSS work.
             </div>
-            <div className={'col-md-6 col-sm-12 col-xs-12 noPadd'}>
-              <div className={'notifyWrapper'}>
-                <SubscribeFrom className={"notifyForm"} {...formProps}/>
+            {/*
+            <div className={'notifyWrapper'}>
+              <div className={'sectionDescription notifyHeader'}>
+                Subscribe for updates
               </div>
+              <SubscribeFrom className={"notifyForm"} {...formProps}/>
+            </div>
+            */}
+          </div>
+          <div className={'col-md-6 col-sm-12 col-xs-12 noPadd'}>
+            <div className={'notifyWrapper'}>
+              <SubscribeFrom className={"notifyForm"} {...formProps}/>
             </div>
           </div>
         </div>
-      </section>
-    );
-  }
+      </div>
+    </section>
+  );
 }
 
 export default SubscribeNewsletter;

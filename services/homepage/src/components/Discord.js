e861e577ef40c4fa6c01aff545f148414dad2b73
Surendran <surendran@hasura.io>
2020-01-10 16:55:02 +0530

    Discord section fix

--- services/homepage/src/components/Discord.js
+++ services/homepage/src/components/Discord.js
@@ -8,10 +8,10 @@ const Discord = () => {
         <div className='container noPadd'>
           <div className='clearBoth alignCenter'>
             <div className='col-md-8 col-sm-8 col-xs-12 noPadd'>
-              <h2>
+              <h1>
                 <img src={discordWhite} alt={'Discord logo'}/>
                 Hasura Discord Community
-              </h2>
+              </h1>
               <p className='pSmall'>If you need any help with developing on Hasura, the 4000 member strong Hasura Discord is here to help. Our community members include some very experienced engineers from some of the world’s most exciting companies, and many of them have been using Hasura in Production for a long time.</p>
               <div className='buttonWrapper'>
                 <a href={'https://discordapp.com/invite/hasura'} target={'_blank'} rel="noopener noreferrer"aria-label="Join the discord">

7fff1e76f78485ba0b2490e15e1628e82a186e31
Praveen Durairaju <praveen@hasura.io>
2020-03-26 12:02:54 +0530

    add intro to graphql course (#171)

---
+++ tutorials/graphql/intro-graphql/tutorial-site/config.js
@@ -0,0 +1,81 @@
+const config = {
+	"gatsby": {
+		"pathPrefix": "/learn/graphql/intro-graphql",
+		"siteUrl": "https://hasura.io",
+		"gaTrackingId": "UA-59768903-1",
+		"trailingSlash": true
+	},
+	"header": {
+		logo: 'https://graphql-engine-cdn.hasura.io/learn-hasura/assets/homepage/brand.svg',
+		"logoLink": "https://hasura.io/learn/",
+		"title": "<a href='https://hasura.io/learn/'>/ intro-to-graphql </a>",
+		title:
+      "<a href='https://hasura.io/learn/'><img class='img-responsive' src='https://graphql-engine-cdn.hasura.io/learn-hasura/assets/homepage/learn-logo.svg' alt='Learn logo' /></a>",
+		"githubUrl": "https://github.com/hasura/learn-graphql",
+		"helpUrl": "",
+		"tweetText": "Check out this Introduction to GraphQL course for Fullstack developers by @HasuraHQ https://hasura.io/learn/graphql/intro-graphql/introduction/",
+		"links": [{
+			"text": "",
+			"link": ""
+		}],
+		social: `<li>
+		    <a href="https://twitter.com/hasurahq" target="_blank" rel="noopener">
+		      <div class="twitterBtn">
+		        <img src='https://graphql-engine-cdn.hasura.io/learn-hasura/assets/homepage/twitter-brands-block.svg' alt={'Discord'}/>
+		      </div>
+		    </a>
+		  </li>
+			<li>
+		    <a href="https://discordapp.com/invite/hasura" target="_blank" rel="noopener">
+		      <div class="discordBtn">
+		        <img src='https://graphql-engine-cdn.hasura.io/learn-hasura/assets/homepage/discord-brands-block.svg' alt={'Discord'}/>
+		      </div>
+		    </a>
+		</li>`,
+		"search": {
+			"enabled": true,
+			"indexName": "learn-intro-graphql",
+			"algoliaAppId": process.env.GATSBY_ALGOLIA_APP_ID,
+			"algoliaSearchKey": process.env.GATSBY_ALGOLIA_SEARCH_KEY,
+			"algoliaAdminKey": process.env.ALGOLIA_ADMIN_KEY
+		}
+	},
+	"sidebar": {
+		"forcedNavOrder": [
+			"/introduction/",
+    		"/what-is-graphql/",
+    		"/graphql-vs-rest/",
+    		"/core-concepts/",
+    		"/introspection/",
+    		"/graphql-queries/",
+    		"/graphql-mutations/",
+    		"/graphql-subscriptions/",
+    		"/graphql-server/",
+    		"/graphql-client/",
+    		"/what-next/"
+    	],
+		"links": [
+			{
+			"text": "Hasura Docs",
+			"link": "https://hasura.io/docs"
+			},
+			{
+			"text": "GraphQL Docs",
+			"link": "https://graphql.org/learn"
+			}
+		],
+		"frontline": false,
+		"ignoreIndex": true,
+		title:
+      "<a href='https://hasura.io/learn/'>GraphQL </a><div class='greenCircle'></div><a href='https://hasura.io/learn/graphql/intro-graphql/introduction/'>Introduction</a>",
+	},
+	"siteMetadata": {
+		"title": "Introduction to GraphQL Tutorial for Fullstack Developers",
+		"description": "A concise and powerful tutorial that covers fundamental concepts of GraphQL, GraphQL vs REST, GraphQL server and client",
+		"ogImage": "https://graphql-engine-cdn.hasura.io/learn-hasura/assets/social-media/twitter-card-intro-graphql.png",
+		"docsLocation": "https://github.com/hasura/learn-graphql/tree/master/tutorials/graphql/intro-graphql/tutorial-site/content",
+		"favicon": "https://graphql-engine-cdn.hasura.io/learn-hasura/assets/graphql-intro/graphql-favicon.png"
+	},
+};
+
+module.exports = config;
---
+++ tutorials/graphql/intro-graphql/tutorial-site/content/introduction.md
@@ -0,0 +1,27 @@
+---
+title: "Course Introduction"
+metaTitle: "Course Introduction | GraphQL Tutorial"
+metaDescription: "A powerful and concise tutorial that will introduce you to GraphQL."
+---
+
+GraphQL is becoming the new way to use APIs in modern web and mobile apps.
+
+However, learning new things always takes time and, without getting your hands dirty, it’s hard to understand the nuances of new technology.
+
+So, we put together a practical and concise course that will introduce you to GraphQL and its core concepts.
+
+We will explore the fundamentals of GraphQL and what makes it especially suitable for modern applications, like its realtime capabilities! 
+
+## Key topics and takeaways:
+
+- Introduction to GraphQL
+- Core Concepts
+- GraphQL vs REST, a comparison
+- Queries
+- Mutations
+- Subscriptions
+- GraphQL Server and Architecture
+- GraphQL Clients
+
+## How long will this tutorial take?
+Less than an hour

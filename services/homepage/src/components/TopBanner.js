d7214ef0c2aa604c34612f0907d96c57bc8b958b
surendran82 <surendran@hasura.io>
2020-05-29 17:23:44 +0530

    homepage: add new course section to banner (#219)

--- services/homepage/src/components/TopBanner.js
+++ services/homepage/src/components/TopBanner.js
@@ -8,13 +8,14 @@ const TopBanner = () => {
   const logo = 'https://graphql-engine-cdn.hasura.io/learn-hasura/assets/homepage/logo.svg';
   const discordBrandsBlock = 'https://graphql-engine-cdn.hasura.io/learn-hasura/assets/homepage/discord-brands-block.svg'
   const twitterBrandsBlock = 'https://graphql-engine-cdn.hasura.io/learn-hasura/assets/homepage/twitter-brands-block.svg'
+  const arrowGreen = require('../images/arrow-forward-green.svg');
   return (
     <div>
       <BannerStripe/>
       <section className='blueBgColor positionRel'>
         <div className='container noPadd'>
           <div className='topBannerWrapper col-md-12 noPadd'>
-            <div className='col-md-6 col-sm-12 col-xs-12 noPadd'>
+            <div className='col-md-7 col-sm-12 col-xs-12 noPadd'>
               <header className='headerWrapper'>
                 <div className='logoWrapper'>
                   <a className="brand" href="https://hasura.io/" target="_blank" rel="noopener noreferrer"><img className='img-responsive' src={logo} alt='Hasura logo' /></a>
@@ -37,8 +38,13 @@ const TopBanner = () => {
                 {/* With these <a href="https://github.com/hasura/learn-graphql">open-source</a> community maintained tutorials, you will move from the basics of GraphQL to building a real-time application in 2 hours.*/}
                 You will move from GraphQL basics to production-ready concepts with our hands-on tutorial for frontend and backend developers.
               </div>
+              <div className='arrowLink'>
+                <a href="/learn/graphql/nextjs-fullstack-serverless/introduction/" target='_blank' rel="noopener noreferrer">
+                  Check out our latest full stack course with Next.js & Hasura<img src={arrowGreen} alt='Arrow'/>
+                </a>
+              </div>
             </div>
-            <div className='col-md-6 col-sm-12 col-xs-12'>
+            <div className='col-md-5 col-sm-12 col-xs-12'>
               <div className='topBannerImg'>
                 <img className='img-responsive' src={hasuraDumbledore} alt='Hasura dumbledore' />
               </div>

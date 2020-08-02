0a182aa33e88458c822e53dc9c7693285734f45c
Gautam BT <git-author@l1cache.in>
2020-08-02 12:16:32 +0530

    React RxDB: Enable search

--- tutorials/frontend/react-rxdb/tutorial-site/config.js
+++ tutorials/frontend/react-rxdb/tutorial-site/config.js
@@ -17,8 +17,8 @@ const config = {
 			"link": "https://hasura.io"
 		}],
 		"search": {
-			"enabled": false,
-			"indexName": "learn-react-apollo",
+			"enabled": true,
+			"indexName": "learn-react-rxdb",
 			"algoliaAppId": process.env.GATSBY_ALGOLIA_APP_ID,
 			"algoliaSearchKey": process.env.GATSBY_ALGOLIA_SEARCH_KEY,
 			"algoliaAdminKey": process.env.ALGOLIA_ADMIN_KEY

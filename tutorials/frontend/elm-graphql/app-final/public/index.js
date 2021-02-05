73da6aa8e0b12a32b7b049aff8842ca95ef03eba
jegor <yagger@gmail.com>
2021-02-05 14:50:58 +0200

    Wrap index.js code into DOMContentLoaded event

--- tutorials/frontend/elm-graphql/app-final/public/index.js
+++ tutorials/frontend/elm-graphql/app-final/public/index.js
@@ -1,12 +1,14 @@
-var app = Elm.Main.init({node: document.getElementById('app')})
+document.addEventListener("DOMContentLoaded", function() {
+    var app = Elm.Main.init({node: document.getElementById('app')})
 
-app.ports.storeToken.subscribe(function(token) {
-    localStorage.setItem('token', token)
+    app.ports.storeToken.subscribe(function(token) {
+        localStorage.setItem('token', token)
+    })
+    app.ports.removeTokenFromStarage.subscribe(function() {
+        localStorage.removeItem('token')
+    })
+    var token = localStorage.getItem('token')
+    if ( token ) {
+        app.ports.gotStoredToken.send(token)
+    }
 })
-app.ports.removeTokenFromStarage.subscribe(function() {
-    localStorage.removeItem('token')
-})
-var token = localStorage.getItem('token')
-if ( token ) {
-    app.ports.gotStoredToken.send(token)
-}

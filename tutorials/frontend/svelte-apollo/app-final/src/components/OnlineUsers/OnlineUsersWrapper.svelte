4cb55a00963a81580426f50725884b665dadd398
akiran <kiran@neostack.com>
2021-06-21 00:00:07 +0530

    online users

--- tutorials/frontend/svelte-apollo/app-final/src/components/OnlineUsers/OnlineUsersWrapper.svelte
+++ tutorials/frontend/svelte-apollo/app-final/src/components/OnlineUsers/OnlineUsersWrapper.svelte
@@ -1,7 +1,9 @@
 <script>
+  import { onMount, onDestroy } from "svelte";
   import OnlineUser from "./OnlineUser.svelte";
   import { gql } from "@apollo/client";
-  import { subscribe } from "svelte-apollo";
+  import { subscribe, mutation } from "svelte-apollo";
+
   const onlineUsers = subscribe(gql`
     subscription getOnlineUsers {
       online_users(order_by: { user: { name: asc } }) {
@@ -12,10 +14,40 @@
       }
     }
   `);
+
+  let onlineIndicator;
+
+  const UPDATE_LASTSEEN_MUTATION = gql`
+    mutation updateLastSeen($now: timestamptz!) {
+      update_users(where: {}, _set: { last_seen: $now }) {
+        affected_rows
+      }
+    }
+  `;
+  const updateLastSeenMutation = mutation(UPDATE_LASTSEEN_MUTATION);
+
+  const updateLastSeen = async () => {
+    // Use the apollo client to run a mutation to update the last_seen value
+    await updateLastSeenMutation({
+      variables: { now: new Date().toISOString() },
+    });
+  };
+
+  onMount(async () => {
+    // Every 20s, run a mutation to tell the backend that you're online
+    await updateLastSeen();
+    onlineIndicator = setInterval(async () => await updateLastSeen(), 20000);
+  });
+
+  onDestroy(() => {
+    clearInterval(onlineIndicator);
+  });
 </script>
 
 {#if $onlineUsers.loading}
   <div>loading ...</div>
+{:else if $onlineUsers.error}
+  <div>Error!</div>
 {:else if $onlineUsers.data}
   <div class="onlineUsersWrapper">
     <div class="sliderHeader">

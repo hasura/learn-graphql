b3d9be06fed2c174e5d95eff2b1c916a5945bf31
akiran <kiran@neostack.com>
2021-06-20 23:49:22 +0530

    users subscription

--- tutorials/frontend/svelte-apollo/app-final/src/components/OnlineUsers/OnlineUsersWrapper.svelte
+++ tutorials/frontend/svelte-apollo/app-final/src/components/OnlineUsers/OnlineUsersWrapper.svelte
@@ -1,11 +1,28 @@
 <script>
   import OnlineUser from "./OnlineUser.svelte";
-  const onlineUsers = [{ name: "someUser1" }, { name: "someUser2" }];
+  import { gql } from "@apollo/client";
+  import { subscribe } from "svelte-apollo";
+  const onlineUsers = subscribe(gql`
+    subscription getOnlineUsers {
+      online_users(order_by: { user: { name: asc } }) {
+        id
+        user {
+          name
+        }
+      }
+    }
+  `);
 </script>
 
-<div class="onlineUsersWrapper">
-  <div class="sliderHeader">Online users - {onlineUsers.length}</div>
-  {#each onlineUsers as user}
-    <OnlineUser {user} />
-  {/each}
-</div>
+{#if $onlineUsers.loading}
+  <div>loading ...</div>
+{:else if $onlineUsers.data}
+  <div class="onlineUsersWrapper">
+    <div class="sliderHeader">
+      Online users - {$onlineUsers.data.online_users.length}
+    </div>
+    {#each $onlineUsers.data.online_users as u (u.id)}
+      <OnlineUser user={u.user} />
+    {/each}
+  </div>
+{/if}

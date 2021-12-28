1bf77c06514077676669c54518be8715e543b345
Gavin Ray <ray.gavin97@gmail.com>
2021-12-28 18:52:51 -0500

    Initial Vue 3 + Apollo 4 setup

---
+++ tutorials/frontend/vue3-apollo/app-boilerplate/src/components/TodoPrivateWrapper.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="todoWrapper">
+    <TodoInput type="private" />
+    <TodoPrivateList type="private" />
+  </div>
+</template>
+
+<script>
+import TodoInput from "../components/TodoInput.vue";
+import TodoPrivateList from "../components/TodoPrivateList.vue";
+export default {
+  components: {
+    TodoInput, TodoPrivateList
+  }
+}
+</script>
---
+++ tutorials/frontend/vue3-apollo/app-boilerplate/src/components/TodoPublicWrapper.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="todoWrapper">
+    <TodoInput type="public" />
+    <TodoPublicList type="public" />
+  </div>
+</template>
+
+<script>
+import TodoInput from "../components/TodoInput.vue";
+import TodoPublicList from "../components/TodoPublicList.vue";
+export default {
+  components: {
+    TodoInput, TodoPublicList
+  },
+}
+</script>

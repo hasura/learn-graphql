0da590b2da81058b1d30008387d62796129a027d
georges nodari <ngeorges.dev@gmail.com>
2022-12-14 15:17:40 +0100

    learn step: subscriptions

--- tutorials/frontend/react-apollo-hooks/app-boilerplate/src/components/Todo/TodoItem.js
+++ tutorials/frontend/react-apollo-hooks/app-boilerplate/src/components/Todo/TodoItem.js
@@ -1,11 +1,33 @@
 import React from "react";
 import { useMutation, gql } from "@apollo/client";
-import {GET_MY_TODOS} from './TodoPrivateList';
+import { GET_MY_TODOS } from './TodoPrivateList';
 
 const TodoItem = ({ index, todo }) => {
+  const REMOVE_TODO = gql`
+  mutation removeTodo ($id: Int!) {
+    delete_todos(where: {id: {_eq: $id}}) {
+      affected_rows
+    }
+  }
+`;
+
+  const [removeTodoMutation] = useMutation(REMOVE_TODO);
+
   const removeTodo = e => {
     e.preventDefault();
     e.stopPropagation();
+    removeTodoMutation({
+      variables: { id: todo.id },
+      optimisticResponse: true,
+      update: (cache) => {
+        const existingTodos = cache.readQuery({ query: GET_MY_TODOS });
+        const newTodos = existingTodos.todos.filter(t => (t.id !== todo.id));
+        cache.writeQuery({
+          query: GET_MY_TODOS,
+          data: { todos: newTodos }
+        });
+      }
+    });
   };
 
   const TOGGLE_TODO = gql`
@@ -25,18 +47,18 @@ const TodoItem = ({ index, todo }) => {
         const existingTodos = cache.readQuery({ query: GET_MY_TODOS });
         const newTodos = existingTodos.todos.map(t => {
           if (t.id === todo.id) {
-            return {...t, is_completed: !t.is_completed};
+            return { ...t, is_completed: !t.is_completed };
           } else {
             return t;
           }
         });
         cache.writeQuery({
           query: GET_MY_TODOS,
-          data: {todos: newTodos}
+          data: { todos: newTodos }
         });
       }
     });
-    
+
   };
 
   return (
--- tutorials/frontend/react-apollo-hooks/app-boilerplate/src/components/Todo/TodoPrivateList.js
+++ tutorials/frontend/react-apollo-hooks/app-boilerplate/src/components/Todo/TodoPrivateList.js
@@ -1,5 +1,5 @@
 import React, { useState, Fragment } from "react";
-import { gql, useQuery } from '@apollo/client';
+import { useQuery, useMutation, gql } from "@apollo/client";
 
 import TodoItem from "./TodoItem";
 import TodoFilters from "./TodoFilters";
@@ -14,6 +14,14 @@ const GET_MY_TODOS = gql`
   }
  }`;
 
+const CLEAR_COMPLETED = gql`
+mutation clearCompleted {
+  delete_todos(where: {is_completed: {_eq: true}, is_public: {_eq: false}}) {
+    affected_rows
+  }
+}
+`;
+
 const TodoPrivateList = props => {
   const [state, setState] = useState({
     filter: "all",
@@ -27,7 +35,18 @@ const TodoPrivateList = props => {
     });
   };
 
-  const clearCompleted = () => { };
+  const [clearCompletedTodos] = useMutation(CLEAR_COMPLETED);
+
+  const clearCompleted = () => {
+    clearCompletedTodos({
+      optimisticResponse: true,
+      update: (cache, { data }) => {
+        const existingTodos = cache.readQuery({ query: GET_MY_TODOS });
+        const newTodos = existingTodos.todos.filter(t => (!t.is_completed));
+        cache.writeQuery({ query: GET_MY_TODOS, data: { todos: newTodos } });
+      }
+    });
+  };
 
   const { todos } = props;
   let filteredTodos = todos;

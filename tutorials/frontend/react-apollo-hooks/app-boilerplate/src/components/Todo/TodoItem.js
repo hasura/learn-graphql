907701e8ad0d172092f4ab9cb4dfbae5969277f0
georges nodari <ngeorges.dev@gmail.com>
2022-12-14 14:50:01 +0100

    add modif task

--- tutorials/frontend/react-apollo-hooks/app-boilerplate/src/components/Todo/TodoItem.js
+++ tutorials/frontend/react-apollo-hooks/app-boilerplate/src/components/Todo/TodoItem.js
@@ -1,4 +1,6 @@
 import React from "react";
+import { useMutation, gql } from "@apollo/client";
+import {GET_MY_TODOS} from './TodoPrivateList';
 
 const TodoItem = ({ index, todo }) => {
   const removeTodo = e => {
@@ -6,7 +8,36 @@ const TodoItem = ({ index, todo }) => {
     e.stopPropagation();
   };
 
-  const toggleTodo = () => {};
+  const TOGGLE_TODO = gql`
+  mutation toggleTodo ($id: Int!, $isCompleted: Boolean!) {
+    update_todos(where: {id: {_eq: $id}}, _set: {is_completed: $isCompleted}) {
+      affected_rows
+    }
+  }
+`;
+  const [toggleTodoMutation] = useMutation(TOGGLE_TODO);
+
+  const toggleTodo = () => {
+    toggleTodoMutation({
+      variables: { id: todo.id, isCompleted: !todo.is_completed },
+      optimisticResponse: true,
+      update: (cache) => {
+        const existingTodos = cache.readQuery({ query: GET_MY_TODOS });
+        const newTodos = existingTodos.todos.map(t => {
+          if (t.id === todo.id) {
+            return {...t, is_completed: !t.is_completed};
+          } else {
+            return t;
+          }
+        });
+        cache.writeQuery({
+          query: GET_MY_TODOS,
+          data: {todos: newTodos}
+        });
+      }
+    });
+    
+  };
 
   return (
     <li>

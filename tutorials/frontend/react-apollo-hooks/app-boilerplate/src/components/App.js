4b605f049daa5cdd9d2fa53e0db3865bffbb3a5f
georges nodari <ngeorges.dev@gmail.com>
2022-12-14 02:32:17 +0100

    learn step: Update todos - mutation

--- tutorials/frontend/react-apollo-hooks/app-boilerplate/src/components/App.js
+++ tutorials/frontend/react-apollo-hooks/app-boilerplate/src/components/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { useAuth0 } from "@auth0/auth0-react";
 
 import Header from "./Header";
@@ -7,11 +7,26 @@ import TodoPrivateWrapper from "./Todo/TodoPrivateWrapper";
 import TodoPublicWrapper from "./Todo/TodoPublicWrapper";
 import OnlineUsersWrapper from "./OnlineUsers/OnlineUsersWrapper";
 
+import { ApolloClient, ApolloProvider, InMemoryCache, HttpLink } from '@apollo/client';
+
 import useAccessToken from "../hooks/useAccessToken";
+
+const createApolloClient = () => {
+  return new ApolloClient({
+    link: new HttpLink({
+      uri: 'https://hasura.io/learn/graphql',
+      headers: {
+        Authorization: `Bearer ${process.env.REACT_APP_AUTH_GRAPHQL}`
+      }
+    }),
+    cache: new InMemoryCache(),
+  });
+};
 
 const App = () => {
   const idToken = useAccessToken();
   const { loading, logout } = useAuth0();
+  const [client] = useState(createApolloClient(idToken));
 
   if (loading) {
     return <div>Loading...</div>;
@@ -21,24 +36,26 @@ const App = () => {
     return <Login />;
   }
   return (
-    <div>
-      <Header logoutHandler={logout} />
-      <div className="row container-fluid p-left-right-0 m-left-right-0">
-        <div className="row col-md-9 p-left-right-0 m-left-right-0">
-          <div className="col-md-6 sliderMenu p-30">
-            <TodoPrivateWrapper />
+    <ApolloProvider client={client}>
+      <div>
+        <Header logoutHandler={logout} />
+        <div className="row container-fluid p-left-right-0 m-left-right-0">
+          <div className="row col-md-9 p-left-right-0 m-left-right-0">
+            <div className="col-md-6 sliderMenu p-30">
+              <TodoPrivateWrapper />
+            </div>
+            <div className="col-md-6 sliderMenu p-30 bg-gray border-right">
+              <TodoPublicWrapper />
+            </div>
           </div>
-          <div className="col-md-6 sliderMenu p-30 bg-gray border-right">
-            <TodoPublicWrapper />
-          </div>
-        </div>
-        <div className="col-md-3 p-left-right-0">
-          <div className="col-md-12 sliderMenu p-30 bg-gray">
-            <OnlineUsersWrapper />
+          <div className="col-md-3 p-left-right-0">
+            <div className="col-md-12 sliderMenu p-30 bg-gray">
+              <OnlineUsersWrapper />
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </ApolloProvider>
   );
 };
 
--- tutorials/frontend/react-apollo-hooks/app-boilerplate/src/components/Todo/TodoPrivateList.js
+++ tutorials/frontend/react-apollo-hooks/app-boilerplate/src/components/Todo/TodoPrivateList.js
@@ -1,26 +1,23 @@
 import React, { useState, Fragment } from "react";
+import { gql, useQuery } from '@apollo/client';
 
 import TodoItem from "./TodoItem";
 import TodoFilters from "./TodoFilters";
+
+const GET_MY_TODOS = gql`
+  query getMyTodos {
+    todos(where: { is_public: { _eq: false} }, order_by: { created_at: desc }) {
+      id
+      title
+      created_at
+      is_completed
+  }
+ }`;
 
 const TodoPrivateList = props => {
   const [state, setState] = useState({
     filter: "all",
     clearInProgress: false,
-    todos: [
-      {
-        id: "1",
-        title: "This is private todo 1",
-        is_completed: true,
-        is_public: false
-      },
-      {
-        id: "2",
-        title: "This is private todo 2",
-        is_completed: false,
-        is_public: false
-      }
-    ]
   });
 
   const filterResults = filter => {
@@ -30,13 +27,14 @@ const TodoPrivateList = props => {
     });
   };
 
-  const clearCompleted = () => {};
+  const clearCompleted = () => { };
 
-  let filteredTodos = state.todos;
+  const { todos } = props;
+  let filteredTodos = todos;
   if (state.filter === "active") {
-    filteredTodos = state.todos.filter(todo => todo.is_completed !== true);
+    filteredTodos = todos.filter(todo => todo.is_completed !== true);
   } else if (state.filter === "completed") {
-    filteredTodos = state.todos.filter(todo => todo.is_completed === true);
+    filteredTodos = todos.filter(todo => todo.is_completed === true);
   }
 
   const todoList = [];
@@ -61,4 +59,19 @@ const TodoPrivateList = props => {
   );
 };
 
-export default TodoPrivateList;
+
+const TodoPrivateListQuery = () => {
+  const { loading, error, data } = useQuery(GET_MY_TODOS);
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+  if (error) {
+    console.error(error);
+    return <div>Error!</div>;
+  }
+  return <TodoPrivateList todos={data.todos} />;
+};
+
+export default TodoPrivateListQuery;
+export { GET_MY_TODOS };

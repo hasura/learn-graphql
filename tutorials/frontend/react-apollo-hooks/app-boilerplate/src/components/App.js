e0d7a8d6fba6e0602f08ff4175f0d92c3f6104ad
georges nodari <ngeorges.dev@gmail.com>
2022-12-14 23:14:04 +0100

    learn:  finish

--- tutorials/frontend/react-apollo-hooks/app-boilerplate/src/components/App.js
+++ tutorials/frontend/react-apollo-hooks/app-boilerplate/src/components/App.js
@@ -7,16 +7,22 @@ import TodoPrivateWrapper from "./Todo/TodoPrivateWrapper";
 import TodoPublicWrapper from "./Todo/TodoPublicWrapper";
 import OnlineUsersWrapper from "./OnlineUsers/OnlineUsersWrapper";
 
-import { ApolloClient, ApolloProvider, InMemoryCache, HttpLink } from '@apollo/client';
+import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
+import { WebSocketLink } from "@apollo/client/link/ws";
 
 import useAccessToken from "../hooks/useAccessToken";
 
 const createApolloClient = () => {
   return new ApolloClient({
-    link: new HttpLink({
-      uri: 'https://hasura.io/learn/graphql',
-      headers: {
-        Authorization: `Bearer ${process.env.REACT_APP_AUTH_GRAPHQL}`
+    link: new WebSocketLink({
+      uri: 'wss://hasura.io/learn/graphql',
+      options: {
+        reconnect: true,
+        connectionParams: {
+          headers: {
+            Authorization: `Bearer ${process.env.REACT_APP_AUTH_GRAPHQL}`
+          }
+        }
       }
     }),
     cache: new InMemoryCache(),
--- tutorials/frontend/react-apollo-hooks/app-boilerplate/src/components/OnlineUsers/OnlineUsersWrapper.js
+++ tutorials/frontend/react-apollo-hooks/app-boilerplate/src/components/OnlineUsers/OnlineUsersWrapper.js
@@ -1,21 +1,74 @@
-import React from "react";
-
+import React, { useEffect, Fragment, useState } from "react";
+import { useMutation, useSubscription, gql } from "@apollo/client";
 import OnlineUser from "./OnlineUser";
 
 const OnlineUsersWrapper = () => {
-  const onlineUsers = [{ name: "someUser1" }, { name: "someUser2" }];
+  const [onlineIndicator, setOnlineIndicator] = useState(0);
+  let onlineUsersList;
+  
+  useEffect(() => {
+    // Every 20s, run a mutation to tell the backend that you're online
+    updateLastSeen();
+    setOnlineIndicator(setInterval(() => updateLastSeen(), 20000));
 
-  const onlineUsersList = [];
-  onlineUsers.forEach((user, index) => {
-    onlineUsersList.push(<OnlineUser key={index} index={index} user={user} />);
-  });
+    return () => {
+      // Clean up
+      clearInterval(onlineIndicator);
+    };
+  }, []);
+  const UPDATE_LASTSEEN_MUTATION = gql`
+   mutation updateLastSeen($now: timestamptz!) {
+     update_users(where: {}, _set: { last_seen: $now }) {
+       affected_rows
+     }
+   }
+ `;
+  const [updateLastSeenMutation] = useMutation(UPDATE_LASTSEEN_MUTATION);
+
+  const updateLastSeen = () => {
+    // Use the apollo client to run a mutation to update the last_seen value
+    updateLastSeenMutation({
+      variables: { now: new Date().toISOString() }
+    });
+  };
+
+  const { loading, error, data } = useSubscription(
+    gql`
+      subscription getOnlineUsers {
+        online_users(order_by: { user: { name: asc } }) {
+          id
+          user {
+            name
+          }
+        }
+      }
+    `
+  );
+
+  if (loading) {
+    return <span>Loading...</span>;
+  }
+  if (error) {
+    console.error(error);
+    return <span>Error!</span>;
+  }
+  if (data) {
+    onlineUsersList = data.online_users.map(u => (
+      <OnlineUser key={u.id} user={u.user} />
+    ));
+  }
 
   return (
     <div className="onlineUsersWrapper">
-      <div className="sliderHeader">Online users - {onlineUsers.length}</div>
-      {onlineUsersList}
+      <Fragment>
+        <div className="sliderHeader">
+          Online users - {onlineUsersList.length}
+        </div>
+        {onlineUsersList}
+      </Fragment>
     </div>
   );
 };
+
 
 export default OnlineUsersWrapper;

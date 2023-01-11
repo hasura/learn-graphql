2685b74489c057c069203002677b8b37d3b9a419
togo-mori <togo.mori@divx.co.jp>
2023-01-11 14:43:33 +0900

    Revert "providerとclientの設定"
    This reverts commit f0902afb800d97ea1d34250ca76eb6e44cfc3577.

--- tutorials/frontend/react-apollo-hooks/app-boilerplate/src/components/App.js
+++ tutorials/frontend/react-apollo-hooks/app-boilerplate/src/components/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import { useAuth0 } from "@auth0/auth0-react";
 
 import Header from "./Header";
@@ -8,29 +8,10 @@ import TodoPublicWrapper from "./Todo/TodoPublicWrapper";
 import OnlineUsersWrapper from "./OnlineUsers/OnlineUsersWrapper";
 
 import useAccessToken from "../hooks/useAccessToken";
-import {
-  ApolloClient,
-  ApolloProvider,
-  InMemoryCache,
-  HttpLink,
-} from "@apollo/client";
-
-const createApolloClient = (authToken) => {
-  return new ApolloClient({
-    link: new HttpLink({
-      uri: "https://powerful-puma-81.hasura.app/v1/graphql",
-      headers: {
-        Authorization: `Bearer ${authToken}`,
-      },
-    }),
-    cache: new InMemoryCache(),
-  });
-};
 
 const App = () => {
   const idToken = useAccessToken();
   const { loading, logout } = useAuth0();
-  const [client] = useState(createApolloClient(idToken));
 
   if (loading) {
     return <div>Loading...</div>;
@@ -40,26 +21,24 @@ const App = () => {
     return <Login />;
   }
   return (
-    <ApolloProvider client={client}>
-      <div>
-        <Header logoutHandler={logout} />
-        <div className="row container-fluid p-left-right-0 m-left-right-0">
-          <div className="row col-md-9 p-left-right-0 m-left-right-0">
-            <div className="col-md-6 sliderMenu p-30">
-              <TodoPrivateWrapper />
-            </div>
-            <div className="col-md-6 sliderMenu p-30 bg-gray border-right">
-              <TodoPublicWrapper />
-            </div>
+    <div>
+      <Header logoutHandler={logout} />
+      <div className="row container-fluid p-left-right-0 m-left-right-0">
+        <div className="row col-md-9 p-left-right-0 m-left-right-0">
+          <div className="col-md-6 sliderMenu p-30">
+            <TodoPrivateWrapper />
           </div>
-          <div className="col-md-3 p-left-right-0">
-            <div className="col-md-12 sliderMenu p-30 bg-gray">
-              <OnlineUsersWrapper />
-            </div>
+          <div className="col-md-6 sliderMenu p-30 bg-gray border-right">
+            <TodoPublicWrapper />
+          </div>
+        </div>
+        <div className="col-md-3 p-left-right-0">
+          <div className="col-md-12 sliderMenu p-30 bg-gray">
+            <OnlineUsersWrapper />
           </div>
         </div>
       </div>
-    </ApolloProvider>
+    </div>
   );
 };
 
